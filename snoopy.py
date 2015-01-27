ecf579261b917c1dd7ce48acb26fffd9fd48b0cc
rshintani <rshintani@gmail.com>
2015-01-26 19:06:33 -0600

    Adding snoopy.py for real though.

---
+++ snoopy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# This is a Python script that is able to look up all the games in the 
+# USHL and display who's working where. 
+
+# Later, I would like to add the functionality of being able to interpret 
+# referees' habits and predict team compatibility. Basically predict
+# game difficulty from the Official's standpoint. 
