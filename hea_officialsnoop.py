5e1e8b332b7fa827f4c9acc7a1c0c32256112e7a
rshintani <rshintani@gmail.com>
2015-02-03 16:15:28 -0600

    Got the snooper working, returns a list of boxscore links

--- hea_officialsnoop.py
+++ hea_officialsnoop.py
@@ -13,3 +13,50 @@ import tabulate
 import re
 import numpy
 
+
+# Opens link and returns a .soup-ed object
+def make_soup(link):
+	handle = urllib2.urlopen(link)
+	soup = BeautifulSoup(handle)
+	return soup
+	handle.close()
+
+# --------------------------------------------------------------------------
+
+# Takes .soup as input and finds all the boxscore links to be searched. 
+def get_boxscore_links(soup):
+
+	boxes = soup.select('tr td a')
+	# for line in boxes:
+	# 	if str(line).startswith('<a href="/men/boxes15.php', 0, 25): 
+		
+
+	box_links = [str(line)[9:-9] for line in boxes if str(line).startswith('<a href="/men/boxes15.php', 0, 25)]
+
+
+	final_box_links = [str('www.hockeyeastonline.com' + box) for box in box_links]
+	return final_box_links
+
+# --------------------------------------------------------------------------
+
+def snoop_sheet(link_to_box):
+	# Takes a list of links in string form from get_boxscores
+
+
+	pass
+# --------------------------------------------------------------------------
+
+
+
+composite_schedule = 'http://www.hockeyeastonline.com/men/schedule/index.php'
+composite_soup = make_soup(composite_schedule)
+boxscore_links = get_boxscore_links(composite_soup)
+
+
+print boxscore_links
+
+
+
+
+
+
